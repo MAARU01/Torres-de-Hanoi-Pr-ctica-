f121c604057919e05ddcc47e670b0974c79ba386
Mario Antonio Ayala Ruiz <92493280+MAARU20@users.noreply.github.com>
2022-12-03 18:18:57 -0600

    Programa Principal

---
+++ main.py
@@ -0,0 +1,18 @@
+from hanoi import *
+def menu () -> int:
+    bienvenida  = """
+    Bievenido a Torres de Hanoi,
+    en este programa se muestran las 
+    diferentes soluciones que existen 
+    para este juego.
+    1. Hanoi Iterativo
+    2. Hanoi Recursivo
+    3. Hanoi Iterativo-Recursivo
+    """
+    print (bienvenida)
+    opt= int (input ("Ingrese el tipo de solución que desea ver: "))
+    return opt
+
+if __name__ == "__main__":
+    tipo = menu ()
+    hanoi_Juego(tipo)
